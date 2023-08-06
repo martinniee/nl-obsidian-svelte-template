5cc358bc0a3008077490ac5ec2e030c0e24abbd6
martinniee <1954924950@qq.com>
2023-08-06 18:43:00 +0800

    chore: configure svelte environment

---
+++ MyComponent.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let variable: number;
+</script>
+
+<div class="number">
+	<span>My number is {variable}!</span>
+</div>
+<h1>Hello svelte on obsidian plugin!!!</h1>
+
+<style>
+	.number {
+		color: red;
+	}
+</style>
